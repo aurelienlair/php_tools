681e69c7074947da717517f634071942f8d79ecb
Aurélien Lair <aurelien.lair@onebip.com>
2016-02-28 22:39:11 +0100

    Update array.php

---
+++ array.php
@@ -0,0 +1,23 @@
+<?php
+
+$arr = [
+    ['a','b','c'],
+    ['d','e','f']
+];
+
+var_export(
+    array_map(
+        function($elements)
+        {
+            return implode(",", $element); 
+        },
+        $arr
+    )
+);
+
+/*
+array (
+  0 => 'a,b,c',
+  1 => 'd,e,f',
+)
+*/
