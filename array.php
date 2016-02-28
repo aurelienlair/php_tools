6e9f935779f080463f3b4b4e6d4d09c77890dc45
Aurélien Lair <aurelien.lair@onebip.com>
2016-02-28 23:24:20 +0100

    Update array.php

--- array.php
+++ array.php
@@ -5,7 +5,7 @@ $arr = [
     ['d','e','f']
 ];
 
-/* ARRAY MAP (apply a callback to all the elements) */ 
+/* ---------------------- ARRAY MAP (apply a callback to all the elements inside the sub arrays) ---------------------------- */ 
 
 var_export(
     array_map(
@@ -22,4 +22,22 @@ array (
   0 => 'a,b,c',
   1 => 'd,e,f',
 )
+*/
+
+/* ---------------------- CALL_USER_FUNC_ARRAY (apply a callback to all the elements) ---------------------------- */ 
+
+print_r(
+    implode(
+        ",\n", 
+        call_user_func_array('array_merge', $arr)
+    )
+);
+
+/*
+a,
+b,
+c,
+d,
+e,
+f
 */
