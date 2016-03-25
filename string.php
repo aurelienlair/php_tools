01aa667f86a669d9603ca658bf1b270a99e7edab
aurelienlair <aurelien.lair@onebip.com>
2016-03-25 16:56:47 +0100

    string: adding indication of closing tag for heredoc

--- string.php
+++ string.php
@@ -6,4 +6,6 @@
     Hi buddy this is a string
     using Heredoc syntax.
     In this string is it allowed to use "" or '' or whatever...
+
+    Pay attention that the closing tag might be left justified
 EOT;
